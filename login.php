6225b23f4ba99ef2beb1685f82fb1379824e130a
ASMA343434 <asma4343431@outlook.com>
2025-03-14 14:04:43 -0800

    Add files via upload
    A

--- login.php
+++ login.php
@@ -6,13 +6,7 @@
     <title>Login</title>
 </head>
 <body>
-    <h2>Login</h2>
-    <?php if (isset($error)) echo "<p style='color: red;'>$error</p>"; ?>
-    <form method="post" action="">
-        <input type="text" name="username" placeholder="Username" required><br><br>
-        <input type="password" name="password" placeholder="Password" required><br><br>
-        <input type="submit" value="Login">
-    </form>
-    <p>Don't have an account? <a href="register.php">Register here</a></p>
+
+    <p>Don't have an account? <a href="">Register here</a></p>
 </body>
 </html>
