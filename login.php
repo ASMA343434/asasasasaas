d75b6d6c8938b2b1b6c8bbc9821aff3df24cc86b
ASMA343434 <asma4343431@outlook.com>
2025-03-14 14:03:34 -0800

    Add files via upload
    A

--- login.php
+++ login.php
@@ -1,25 +1,4 @@
-<?php
-session_start();
-require_once 'config.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    $sql = "SELECT * FROM users WHERE username = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([$username]);
-    $user = $stmt->fetch();
-
-    if ($user && password_verify($password, $user['password'])) {
-        $_SESSION['user_id'] = $user['id'];
-        header("Location: home.php");
-        exit();
-    } else {
-        $error = "Invalid username or password";
-    }
-}
-?>
 
 <!DOCTYPE html>
 <html>
